220a70cf4483461025d5132a30a5e0e3436768cf
Hirofumi Tsuzuki <hrfm.tsuzuki@gmail.com>
2020-12-05 16:30:46 +0000

    add parser

--- src/gitlab_actions/jobs.py
+++ src/gitlab_actions/jobs.py
@@ -1,7 +1,9 @@
-from typing import Any, Dict, List
+from typing import Any, Dict, List, TextIO
 
+import yaml
 from gitlab.v4.objects import Project
 
+from . import actions as act
 from .actions import BaseAction
 from .targets import BaseTarget, IssueTarget, MergeRequestTarget, TargetType
 
@@ -9,20 +11,20 @@ from .targets import BaseTarget, IssueTarget, MergeRequestTarget, TargetType
 class JobRunner:
     def __init__(
         self,
-        project: Project,
+        name: str,
         target_type: TargetType,
         conditions: Dict[str, Any],
         actions: List[BaseAction],
     ) -> None:
-        self.project: Project = project
+        self.name: str = name
         self.target_type: TargetType = target_type
         self.conditions: Dict[str, Any] = conditions
         self.actions: List[BaseAction] = actions
 
-    def _get_filtered_targets(self) -> List[BaseTarget]:
+    def _get_filtered_targets(self, project: Project) -> List[BaseTarget]:
         target_manager = {
-            TargetType.ISSUE: self.project.issues,
-            TargetType.MERGE_REQUEST: self.project.mergerequests,
+            TargetType.ISSUE: project.issues,
+            TargetType.MERGE_REQUEST: project.mergerequests,
         }[self.target_type]
         native_targets = target_manager.list(all=True, **self.conditions)
 
@@ -36,9 +38,35 @@ class JobRunner:
 
         return targets
 
-    def run(self) -> None:
-        targets: List[BaseTarget] = self._get_filtered_targets()
+    def run(self, project: Project) -> None:
+        targets: List[BaseTarget] = self._get_filtered_targets(project)
 
         for action in self.actions:
             for target in targets:
                 action.run(target)
+
+
+class JobParser:
+    def __init__(self, job_file: TextIO) -> None:
+        self.raw_data: Dict[str, Any] = yaml.safe_load(job_file)
+        self.jobs: List[JobRunner] = []
+
+    def _action_mapper(self, action: Dict[str, Any]) -> BaseAction:
+        action_type = action["type"]
+        try:
+            action_class = getattr(act, action_type.title() + "Action")
+        except AttributeError:
+            raise ValueError(f'action type "{action_type}" is not supported')
+        return action_class(**action["property"])
+
+    def _parse(self, job: Dict[str, Any]) -> JobRunner:
+        return JobRunner(
+            name=job["name"],
+            target_type=TargetType(job["target"]),
+            conditions=job["filters"],
+            actions=[self._action_mapper(action) for action in job["actions"]],
+        )
+
+    def parse(self) -> None:
+        for job in self.raw_data["jobs"]:
+            self.jobs.append(self._parse(job))
