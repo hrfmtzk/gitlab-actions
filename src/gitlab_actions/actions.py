fb43d30949a9ff3e0f2da2b08e33592381527823
Hirofumi Tsuzuki <hrfm.tsuzuki@gmail.com>
2020-12-05 12:59:39 +0000

    fix runner

--- src/gitlab_actions/actions.py
+++ src/gitlab_actions/actions.py
@@ -1,9 +1,98 @@
+from typing import Dict, List, Optional
+
+from gitlab.v4.objects import Project
+
 from .targets import BaseTarget
 
 
 class BaseAction:
-    def __init__(self, target) -> None:
-        self.target = target
-
     def run(self, target: BaseTarget) -> None:
         raise NotImplementedError
+
+
+class StateAction(BaseAction):
+    CLOSE = "close"
+    REOPEN = "reopen"
+
+    def __init__(self, state: str) -> None:
+        self.state = state
+
+    def run(self, target: BaseTarget) -> None:
+        target._native_target.state_event = self.state
+        target._native_target.save()
+
+
+class LabelAction(BaseAction):
+    def __init__(
+        self,
+        labels: Optional[List[str]] = None,
+        add_labels: Optional[List[str]] = None,
+        remove_labels: Optional[List[str]] = None,
+    ) -> None:
+        if labels and (add_labels or remove_labels):
+            raise ValueError("can't accept labels and add/remove labels")
+        self.labels = labels
+        self.add_labels = add_labels
+        self.remove_labels = remove_labels
+
+    def run(self, target: BaseTarget) -> None:
+        if self.labels:
+            target._native_target.labels = self.labels
+        else:
+            target._native_target.add_labels = self.add_labels
+            target._native_target.remove_labels = self.remove_labels
+        target._native_target.save()
+
+
+class AssigneeAction(BaseAction):
+    _member_cache: Dict[str, int] = {}
+
+    def __init__(
+        self, project: Project, username: Optional[str] = None
+    ) -> None:
+        if username and username not in self._member_cache:
+            members = project.members.list(username=username)
+            if not members:
+                raise ValueError(f"{username} is not a project member")
+            self._member_cache[username] = members[0].id
+        self.username = username
+
+    def run(self, target: BaseTarget) -> None:
+        if self.username:
+            target._native_target.assignee_ids = [
+                self._member_cache[self.username]
+            ]
+        else:
+            target._native_target.assignee_ids = []
+        target._native_target.save()
+
+
+class MilestoneAction(BaseAction):
+    _milestone_cache: Dict[str, int] = {}
+
+    def __init__(
+        self, project: Project, milestone: Optional[str] = None
+    ) -> None:
+        if milestone and milestone not in self._milestone_cache:
+            milestones = project.milestones.list(title=milestone)
+            if not milestones:
+                raise ValueError(f"{milestone} does not exist")
+            self._milestone_cache[milestone] = milestones[0].id
+        self.milestone = milestone
+
+    def run(self, target: BaseTarget) -> None:
+        if self.milestone:
+            target._native_target.milestone_id = [
+                self._milestone_cache[self.milestone]
+            ]
+        else:
+            target._native_target.milestone_id = None
+        target._native_target.save()
+
+
+class NoteAction(BaseAction):
+    def __init__(self, body: str) -> None:
+        self.body = body
+
+    def run(self, target: BaseTarget) -> None:
+        target._native_target.notes.create({"body": self.body})
--- src/gitlab_actions/jobs.py
+++ src/gitlab_actions/jobs.py
@@ -1,9 +1,8 @@
-from typing import List
+from typing import Any, Dict, List
 
 from gitlab.v4.objects import Project
 
 from .actions import BaseAction
-from .filters import BaseFilter
 from .targets import BaseTarget, IssueTarget, MergeRequestTarget, TargetType
 
 
@@ -12,25 +11,20 @@ class JobRunner:
         self,
         project: Project,
         target_type: TargetType,
-        filters: List[BaseFilter],
+        conditions: Dict[str, Any],
         actions: List[BaseAction],
     ) -> None:
         self.project: Project = project
         self.target_type: TargetType = target_type
-        self.filters: List[BaseFilter] = filters
+        self.conditions: Dict[str, Any] = conditions
         self.actions: List[BaseAction] = actions
 
     def _get_filtered_targets(self) -> List[BaseTarget]:
-        list_conditions = {
-            filter.list_condition_key: filter.list_condition_value
-            for filter in self.filters
-            if filter.support_list_condition
-        }
         target_manager = {
             TargetType.ISSUE: self.project.issues,
             TargetType.MERGE_REQUEST: self.project.mergerequests,
         }[self.target_type]
-        native_targets = target_manager.list(**list_conditions)
+        native_targets = target_manager.list(all=True, **self.conditions)
 
         target_class = {
             TargetType.ISSUE: IssueTarget,
@@ -39,11 +33,6 @@ class JobRunner:
         targets = [
             target_class(native_target) for native_target in native_targets
         ]
-
-        for filter in self.filters:
-            if filter.support_list_condition:
-                continue
-            targets = filter.filter(targets)
 
         return targets
 
